f5a43774a21c8f27b6d0efb21bf09452a9ef8f0f
niels26 <151180160+Kafkarcom@users.noreply.github.com>
2024-07-31 07:57:54 +0200

    Add logger function and page

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from routes import register_blueprints
 from logger import Logger
-
+import time
+import threading
+from datetime import datetime
 
 app = Flask(__name__)
 logger = Logger(app)
@@ -9,5 +11,18 @@ logger = Logger(app)
 # Register blueprints
 register_blueprints(app)
 
+def log_time():
+    while True:
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        logger.emit_log(f"Current time: {current_time}")
+        time.sleep(10)
+
+if __name__ == '__main__':
+    time_logger_thread = threading.Thread(target=log_time)
+    time_logger_thread.daemon = True
+    time_logger_thread.start()
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
--- routes/log_routes.py
+++ routes/log_routes.py
@@ -2,7 +2,6 @@ from flask import Blueprint, render_template
 
 bp = Blueprint('log', __name__)
 
-def register_log_routes(app):
-    @app.route('/debug_log')
-    def debug_log():
-        return render_template('debug_log.html')
+@bp.route('/debug_log')
+def debug_log():
+    return render_template('debug_log.html')
